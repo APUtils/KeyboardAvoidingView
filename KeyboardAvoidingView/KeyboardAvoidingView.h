800e6abd4db6c4afafe56be90ec7a92610f562aa
Anton Plebanovich <anton.plebanovich@gmail.com>
2018-02-15 20:12:36 +0300

    - 2.2.1 - Carthage support

---
+++ KeyboardAvoidingView/KeyboardAvoidingView.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAvoidingView.h
+//  KeyboardAvoidingView
+//
+//  Created by mac-246 on 2/15/18.
+//  Copyright © 2018 Anton Plebanovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAvoidingView.
+FOUNDATION_EXPORT double KeyboardAvoidingViewVersionNumber;
+
+//! Project version string for KeyboardAvoidingView.
+FOUNDATION_EXPORT const unsigned char KeyboardAvoidingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAvoidingView/PublicHeader.h>
+
+
