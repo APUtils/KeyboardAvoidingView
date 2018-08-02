076ce4280b6c9bb6d832386c0f70c64229db3a70
Anton Plebanovich <anton.plebanovich@gmail.com>
2018-08-02 16:33:22 +0300

    - Comment about import conflict

--- KeyboardAvoidingView/Classes/KeyboardAvoidingViewLoader.m
+++ KeyboardAvoidingView/Classes/KeyboardAvoidingViewLoader.m
@@ -5,6 +5,14 @@
 //  Created by mac-246 on 2/16/18.
 //
 
+// TODO: Is it possible with preprocess macros to use proper import statement?
+
+// Dynamic framework import
+//#import <KeyboardAvoidingView/KeyboardAvoidingView-Swift.h>
+
+// Static framework import
+//#import "KeyboardAvoidingView-Swift.h"
+
 #import "KeyboardAvoidingViewLoader.h"
 
 @implementation KeyboardAvoidingViewLoader
@@ -13,6 +21,7 @@
     Class keyboardManagerClass = NSClassFromString(@"_TtC20KeyboardAvoidingView15KeyboardManager");
     SEL sharedSelector = NSSelectorFromString(@"shared");
     if ([keyboardManagerClass respondsToSelector:sharedSelector]) {
+    // This causes a leak but I don't know how to support both static and dynamic frameworks together.
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
         [keyboardManagerClass performSelector:sharedSelector];
